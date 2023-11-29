829d191708ddfb69cc7e06211d8b2e1184d940d1
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-11-30 00:27:09 +0300

    Add string and bool conversions to Converter class

--- src/FileSystem/Converter/Converter.cpp
+++ src/FileSystem/Converter/Converter.cpp
@@ -27,3 +27,37 @@ auto Converter::to_uint64(const std::vector<std::byte> &bytes) -> std::uint64_t 
 
   return value;
 }
+
+auto Converter::to_bytes(std::string const &value, std::uint64_t size) -> std::vector<std::byte> {
+  std::vector<std::byte> bytes;
+  for (char character : value) bytes.push_back(static_cast<std::byte>(character));
+
+  if (size != 0) {
+    if (bytes.size() > size) throw std::runtime_error("Invalid string size");
+    bytes.resize(size, std::byte{0});
+  }
+
+  return bytes;
+}
+
+auto Converter::to_string(std::vector<std::byte> const &bytes) -> std::string {
+  std::string string;
+  for (std::byte byte : bytes) string.push_back(static_cast<char>(byte));
+
+  return string;
+}
+
+auto Converter::to_bytes(bool value) -> std::vector<std::byte> {
+  std::vector<std::byte> bytes;
+  bytes.push_back(static_cast<std::byte>(value));
+
+  return bytes;
+}
+
+auto Converter::to_bool(std::vector<std::byte> const &bytes) -> bool {
+  if (bytes.size() != 1) throw std::runtime_error("Invalid bytes size");
+
+  if (bytes[0] == std::byte{0}) return false;
+  if (bytes[0] == std::byte{1}) return true;
+  throw std::runtime_error("Invalid bool value");
+}
--- src/FileSystem/Converter/Converter.hpp
+++ src/FileSystem/Converter/Converter.hpp
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <string>
 #include <vector>
 
 class Converter {
 public:
+  // uint64_t
   static auto to_bytes(std::uint64_t value) -> std::vector<std::byte>;
   static auto to_uint64(const std::vector<std::byte> &bytes) -> std::uint64_t;
+
+  // string
+  static auto to_bytes(std::string const &value, std::uint64_t size = 0) -> std::vector<std::byte>;
+  static auto to_string(std::vector<std::byte> const &bytes) -> std::string;
+
+  // bool
+  static auto to_bytes(bool value) -> std::vector<std::byte>;
+  static auto to_bool(std::vector<std::byte> const &bytes) -> bool;
 };
