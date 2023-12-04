02316c29dca30f48853793f80f71eb456a74da7d
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-12-04 21:39:56 +0300

    Refactor FATTest setup and teardown methods

--- test/fat.cpp
+++ test/fat.cpp
@@ -5,30 +5,30 @@ class FATTest : public testing::Test {
 protected:
   // NOLINTBEGIN(cppcoreguidelines-non-private-member-variables-in-classes)
   std::shared_ptr<FAT> fat_;
-  const std::string path = "fat_test.fs";
-  const std::uint64_t size = 1024;
-  const std::uint64_t cluster_size = 64;
+  const std::string PATH = "fat_test.fs";
+  const std::uint64_t SIZE = 1024;
+  const std::uint64_t CLUSTER_SIZE = 64;
   // NOLINTEND(cppcoreguidelines-non-private-member-variables-in-classes)
 
   void SetUp() override {
-    FileSystem::make(path, {size, cluster_size});
+    FileSystem::make(PATH, {SIZE, CLUSTER_SIZE});
 
-    auto ifs = std::make_unique<std::ifstream>(path, std::ios::binary | std::ios::in);
-    auto ofs = std::make_unique<std::ofstream>(path, std::ios::binary | std::ios::out | std::ios::in);
-    if (!ifs->is_open() || !ofs->is_open()) throw std::runtime_error("Cannot open file " + path);
+    auto ifs = std::make_unique<std::ifstream>(PATH, std::ios::binary | std::ios::in);
+    auto ofs = std::make_unique<std::ofstream>(PATH, std::ios::binary | std::ios::out | std::ios::in);
+    if (!ifs->is_open() || !ofs->is_open()) throw std::runtime_error("Cannot open file " + PATH);
 
     auto disk_reader = std::make_shared<DiskReader>(std::move(ifs), DiskHandler::DiskOffset(0));
     auto disk_writer = std::make_shared<DiskWriter>(std::move(ofs), DiskHandler::DiskOffset(0));
 
     fat_ = std::make_shared<FAT>(disk_reader, disk_writer, DiskHandler::DiskOffset(FSMaker::fat_offset()),
-                                 FSMaker::calculate_fat_entries_count({size, cluster_size}));
+                                 FSMaker::calculate_fat_entries_count({SIZE, CLUSTER_SIZE}));
   }
 
-  void TearDown() override { std::filesystem::remove(path); }
+  void TearDown() override { std::filesystem::remove(PATH); }
 };
 
 TEST_F(FATTest, EntriesCount) {
-  EXPECT_EQ(fat_->entries_count(), FSMaker::calculate_fat_entries_count({size, cluster_size}));
+  EXPECT_EQ(fat_->entries_count(), FSMaker::calculate_fat_entries_count({SIZE, CLUSTER_SIZE}));
 }
 
 TEST_F(FATTest, Allocate) {
