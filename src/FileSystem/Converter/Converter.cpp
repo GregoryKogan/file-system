037886541621a27ff53dabd3f4280d76ba525d81
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-12-09 18:55:08 +0300

    Refactor Converter class to use a constant for UINT64_SIZE

--- src/FileSystem/Converter/Converter.cpp
+++ src/FileSystem/Converter/Converter.cpp
@@ -1,32 +1,31 @@
 #include "Converter.hpp"
 
 auto Converter::to_bytes(std::uint64_t value) -> std::vector<std::byte> {
-  const uint8_t BYTES_IN_UINT64 = 8;
   const uint8_t BITS_IN_BYTE = 8;
 
   std::vector<std::byte> bytes;
-  bytes.resize(BYTES_IN_UINT64);
+  bytes.resize(UINT64_SIZE);
 
-  for (std::size_t i = 0; i < BYTES_IN_UINT64; ++i) {
-    bytes[BYTES_IN_UINT64 - i - 1] = static_cast<std::byte>(value >> BITS_IN_BYTE * i);
+  for (std::size_t i = 0; i < UINT64_SIZE; ++i) {
+    bytes[UINT64_SIZE - i - 1] = static_cast<std::byte>(value >> BITS_IN_BYTE * i);
   }
 
   return bytes;
 }
 
 auto Converter::to_uint64(const std::vector<std::byte> &bytes) -> std::uint64_t {
-  const uint8_t BYTES_IN_UINT64 = 8;
-
-  if (bytes.size() != BYTES_IN_UINT64) throw std::invalid_argument("Invalid bytes size");
+  if (bytes.size() != UINT64_SIZE) throw std::invalid_argument("Invalid bytes size");
 
   std::uint64_t value = 0;
 
-  for (std::size_t i = 0; i < BYTES_IN_UINT64; ++i) {
-    value |= static_cast<std::uint64_t>(bytes[BYTES_IN_UINT64 - i - 1]) << BYTES_IN_UINT64 * i;
+  for (std::size_t i = 0; i < UINT64_SIZE; ++i) {
+    value |= static_cast<std::uint64_t>(bytes[UINT64_SIZE - i - 1]) << UINT64_SIZE * i;
   }
 
   return value;
 }
+
+auto Converter::get_uint64_size() noexcept -> std::uint64_t { return UINT64_SIZE; }
 
 auto Converter::to_bytes(std::string const &value, std::uint64_t size) -> std::vector<std::byte> {
   std::vector<std::byte> bytes;
--- src/FileSystem/Converter/Converter.hpp
+++ src/FileSystem/Converter/Converter.hpp
@@ -6,10 +6,13 @@
 #include <vector>
 
 class Converter {
+  static const std::uint64_t UINT64_SIZE = 8;
+
 public:
   // uint64_t
   [[nodiscard]] static auto to_bytes(std::uint64_t value) -> std::vector<std::byte>;
   [[nodiscard]] static auto to_uint64(const std::vector<std::byte> &bytes) -> std::uint64_t;
+  [[nodiscard]] static auto get_uint64_size() noexcept -> std::uint64_t;
 
   // string
   [[nodiscard]] static auto to_bytes(std::string const &value, std::uint64_t size = 0) -> std::vector<std::byte>;
