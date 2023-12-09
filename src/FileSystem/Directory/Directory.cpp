6836ac3446f5b88aafd28e6d68fcd452ada087c8
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-12-09 18:55:41 +0300

    Refactor Directory class to use child cluster indices

--- src/FileSystem/Directory/Directory.cpp
+++ src/FileSystem/Directory/Directory.cpp
@@ -1,67 +1,33 @@
 #include "Directory.hpp"
 
-Directory::Directory(std::vector<FileData> files) : files_(std::move(files)) {}
-
 auto Directory::from_bytes(std::vector<std::byte> const &bytes) -> Directory {
-  auto file_data_size = FileData::file_data_size();
-  if (bytes.size() % file_data_size != 0) { throw std::runtime_error("Invalid directory size"); }
+  auto directory = Directory();
 
-  auto file_count = bytes.size() / file_data_size;
-  auto files = std::vector<FileData>{};
-  files.reserve(file_count);
+  auto step = static_cast<int>(Converter::get_uint64_size());
+  auto cur_offset = 0;
+  while (cur_offset < bytes.size()) {
+    auto cluster_bytes = std::vector<std::byte>(bytes.begin() + cur_offset, bytes.begin() + cur_offset + step);
+    auto cluster = Converter::to_uint64(cluster_bytes);
+    cur_offset += step;
 
-  for (auto i = 0; i < file_count; i++) {
-    auto file_bytes = std::vector<std::byte>(bytes.begin() + static_cast<int>(i * file_data_size),
-                                             bytes.begin() + static_cast<int>((i + 1) * file_data_size));
-    files.push_back(FileData::from_bytes(file_bytes));
+    directory.add_file(cluster);
   }
 
-  return Directory(files);
+  return directory;
 }
 
 auto Directory::to_bytes() const -> std::vector<std::byte> {
   auto bytes = std::vector<std::byte>{};
-  bytes.reserve(files_.size() * FileData::file_data_size());
+  bytes.reserve(child_clusters_.size() * Converter::get_uint64_size());
 
-  for (auto const &file : files_) {
-    auto file_bytes = file.to_bytes();
-    bytes.insert(bytes.end(), file_bytes.begin(), file_bytes.end());
+  for (auto cluster : child_clusters_) {
+    auto cluster_bytes = Converter::to_bytes(cluster);
+    bytes.insert(bytes.end(), cluster_bytes.begin(), cluster_bytes.end());
   }
 
   return bytes;
 }
 
-auto Directory::files() const -> std::vector<FileData> { return files_; }
+auto Directory::list_files() const -> std::vector<std::uint64_t> { return child_clusters_; }
 
-auto Directory::find(std::string const &name) const -> std::optional<FileData> {
-  for (auto const &file : files_) {
-    if (file.name() == name) { return file; }
-  }
-  return {};
-}
-
-auto Directory::add_file(FileData const &file) -> void {
-  if (find(file.name()).has_value()) throw std::runtime_error("File already exists");
-  files_.push_back(file);
-
-  files_[0].set_size(FileData::FileSize{FileData::file_data_size() * files_.size()});
-  if (files_[0].first_cluster_index() == files_[1].first_cluster_index()) {
-    files_[1].set_size(FileData::FileSize{FileData::file_data_size() * files_.size()});
-  }
-}
-
-auto Directory::make_root() -> Directory {
-  auto parent_dir = FileData("..", FileData::FileSize{0}, 0, true);
-  return make_empty(parent_dir, 0);
-}
-
-auto Directory::make_empty(const FileData &parent_dir, std::uint64_t cluster_index) -> Directory {
-  if (!parent_dir.is_directory()) throw std::runtime_error("Cannot create directory in file");
-
-  auto current_dir = FileData(".", FileData::FileSize{FileData::file_data_size() * 2}, cluster_index, true);
-  auto parent_dir_ref = FileData("..", FileData::FileSize{0}, parent_dir.first_cluster_index(), true);
-
-  auto files = std::vector<FileData>{current_dir, parent_dir_ref};
-
-  return Directory(files);
-}
+auto Directory::add_file(std::uint64_t cluster) -> void { child_clusters_.push_back(cluster); }
--- src/FileSystem/Directory/Directory.hpp
+++ src/FileSystem/Directory/Directory.hpp
@@ -1,23 +1,20 @@
 #pragma once
 
-#include "../FileData/FileData.hpp"
-#include <iostream>
 #include <optional>
 
+#include "../Converter/Converter.hpp"
+#include "../Metadata/Metadata.hpp"
+
 class Directory {
-  std::vector<FileData> files_;
+  std::vector<std::uint64_t> child_clusters_;
 
 public:
-  explicit Directory(std::vector<FileData> files);
+  Directory() = default;
 
   static auto from_bytes(std::vector<std::byte> const &bytes) -> Directory;
   [[nodiscard]] auto to_bytes() const -> std::vector<std::byte>;
 
-  [[nodiscard]] auto files() const -> std::vector<FileData>;
-  [[nodiscard]] auto find(std::string const &name) const -> std::optional<FileData>;
+  [[nodiscard]] auto list_files() const -> std::vector<std::uint64_t>;
 
-  auto add_file(FileData const &file) -> void;
-
-  [[nodiscard]] static auto make_root() -> Directory;
-  [[nodiscard]] static auto make_empty(const FileData &parent_dir, std::uint64_t cluster_index) -> Directory;
+  auto add_file(std::uint64_t cluster) -> void;
 };
