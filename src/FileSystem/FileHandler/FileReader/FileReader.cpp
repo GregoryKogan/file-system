9652070c02702fe4a48d181e4231aee14930431b
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-12-09 20:52:55 +0300

    Refactor FileReader class to improve readability and performance

--- src/FileSystem/FileHandler/FileReader/FileReader.cpp
+++ src/FileSystem/FileHandler/FileReader/FileReader.cpp
@@ -1,35 +1,27 @@
 #include "FileReader.hpp"
 
-FileReader::FileReader(FileData file_data, FileOffset offset, std::shared_ptr<FAT> fat, std::uint64_t cluster_size,
-                       std::shared_ptr<DiskReader> disk_reader, BlockSize block_size)
-    : FileHandler(std::move(file_data), offset, std::move(fat), cluster_size), disk_reader_(std::move(disk_reader)),
-      block_size_(block_size.value) {}
+FileReader::FileReader(std::uint64_t first_cluster, std::uint64_t offset, std::shared_ptr<FAT> fat,
+                       std::uint64_t cluster_size, std::uint64_t clusters_start_offset,
+                       std::shared_ptr<DiskReader> disk_reader, std::uint64_t block_size)
+    : FileHandler(first_cluster, offset, std::move(fat), cluster_size), disk_reader_(std::move(disk_reader)),
+      block_size_(block_size), clusters_start_offset_(clusters_start_offset) {}
 
-auto FileReader::block_size() const noexcept -> std::uint64_t { return block_size_; }
+auto FileReader::get_block_size() const noexcept -> std::uint64_t { return block_size_; }
 
-void FileReader::set_block_size(BlockSize block_size) noexcept { block_size_ = block_size.value; }
+void FileReader::set_block_size(std::uint64_t block_size) noexcept { block_size_ = block_size; }
+
+auto FileReader::read_metadata() const -> Metadata {
+  auto metadata_bytes = read_bytes(0, Metadata::get_metadata_size());
+  return Metadata::from_bytes(metadata_bytes);
+}
 
 auto FileReader::read_block() const -> std::vector<std::byte> {
-  auto out_size = std::min(block_size_, file_data().size().bytes - offset() - handled_size());
+  auto meta = read_metadata();
 
-  std::vector<std::byte> block;
-  block.reserve(out_size);
+  auto size = std::min(block_size_, meta.get_size() - get_offset() - get_handled_size());
+  auto offset = Metadata::get_metadata_size() + get_offset() + get_handled_size();
 
-  auto cur_cluster = file_data().first_cluster_index();
-  std::uint64_t file_position = 0;
-
-  while (block.size() < out_size) {
-    auto cluster_bytes = read_cluster(cur_cluster);
-    for (auto byte : cluster_bytes) {
-      if (file_position >= offset() + handled_size()) { block.push_back(byte); }
-      ++file_position;
-      if (block.size() >= out_size) { break; }
-    }
-
-    if (block.size() < out_size) { cur_cluster = fat()->get_next(cur_cluster); }
-  }
-
-  return block;
+  return read_bytes(offset, size);
 }
 
 auto FileReader::read_next_block() -> std::vector<std::byte> {
@@ -38,11 +30,30 @@ auto FileReader::read_next_block() -> std::vector<std::byte> {
   return block;
 }
 
-auto FileReader::read_cluster(std::uint64_t cluster_index) const -> std::vector<std::byte> {
-  auto cluster_offset = fat()->cluster_offset(cluster_index, cluster_size());
-  disk_reader_->set_offset(cluster_offset);
-  disk_reader_->set_block_size(DiskReader::BlockSize(cluster_size()));
-  return disk_reader_->read_block();
+auto FileReader::read_bytes(std::uint64_t offset, std::uint64_t size) const -> std::vector<std::byte> {
+  std::vector<std::byte> bytes;
+  bytes.reserve(size);
+
+  auto cur_cluster = get_first_cluster();
+  std::uint64_t file_position = 0;
+
+  while (bytes.size() < size) {
+    auto cluster_bytes = read_cluster(cur_cluster);
+    for (auto byte : cluster_bytes) {
+      if (file_position >= offset) { bytes.push_back(byte); }
+      ++file_position;
+      if (bytes.size() >= size) { break; }
+    }
+
+    if (bytes.size() < size) { cur_cluster = get_fat()->get_next(cur_cluster); }
+  }
+
+  return bytes;
 }
 
-FileReader::BlockSize::BlockSize(std::uint64_t value) : value(value) {}
+auto FileReader::read_cluster(std::uint64_t cluster_index) const -> std::vector<std::byte> {
+  auto cluster_offset = clusters_start_offset_ + cluster_index * get_cluster_size();
+  disk_reader_->set_offset(cluster_offset);
+  disk_reader_->set_block_size(get_cluster_size());
+  return disk_reader_->read_block();
+}
--- src/FileSystem/FileHandler/FileReader/FileReader.hpp
+++ src/FileSystem/FileHandler/FileReader/FileReader.hpp
@@ -7,25 +7,20 @@ class FileReader : public FileHandler {
   std::uint64_t block_size_;
   std::shared_ptr<DiskReader> disk_reader_;
 
+  std::uint64_t clusters_start_offset_;
+
 public:
-  struct BlockSize {
-    std::uint64_t value;
+  FileReader(std::uint64_t first_cluster, std::uint64_t offset, std::shared_ptr<FAT> fat, std::uint64_t cluster_size,
+             std::uint64_t clusters_start_offset, std::shared_ptr<DiskReader> disk_reader, std::uint64_t block_size);
 
-    explicit BlockSize(std::uint64_t value);
-  };
+  [[nodiscard]] auto get_block_size() const noexcept -> std::uint64_t;
+  void set_block_size(std::uint64_t block_size) noexcept;
 
-  static const std::uint64_t DEFAULT_BLOCK_SIZE = 1024;
-
-  FileReader(FileData file_data, FileOffset offset, std::shared_ptr<FAT> fat, std::uint64_t cluster_size,
-             std::shared_ptr<DiskReader> disk_reader, BlockSize block_size = BlockSize(DEFAULT_BLOCK_SIZE));
-
-  [[nodiscard]] auto block_size() const noexcept -> std::uint64_t;
-
-  void set_block_size(BlockSize block_size) noexcept;
-
+  [[nodiscard]] auto read_metadata() const -> Metadata;
   [[nodiscard]] auto read_block() const -> std::vector<std::byte>;
   auto read_next_block() -> std::vector<std::byte>;
 
 private:
+  [[nodiscard]] auto read_bytes(std::uint64_t offset, std::uint64_t size) const -> std::vector<std::byte>;
   [[nodiscard]] auto read_cluster(std::uint64_t cluster_index) const -> std::vector<std::byte>;
 };
