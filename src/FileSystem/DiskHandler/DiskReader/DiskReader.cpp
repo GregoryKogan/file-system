db66377deb48d8a10525570b256c01eace5fd197
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-12-09 18:55:31 +0300

    Refactor DiskReader class to use uint64_t for block size
    This commit refactors the DiskReader class to use std::uint64_t for the block size parameter. It updates the constructor, getter, and setter methods accordingly. This change improves the clarity and consistency of the code.

--- src/FileSystem/DiskHandler/DiskReader/DiskReader.cpp
+++ src/FileSystem/DiskHandler/DiskReader/DiskReader.cpp
@@ -1,22 +1,22 @@
 #include "DiskReader.hpp"
 
-DiskReader::DiskReader(std::unique_ptr<std::istream> stream, DiskOffset offset, BlockSize block_size)
-    : DiskHandler(offset), is_(std::move(stream)), block_size_(block_size.value) {}
+DiskReader::DiskReader(std::unique_ptr<std::istream> stream, std::uint64_t offset, std::uint64_t block_size)
+    : DiskHandler(offset), is_(std::move(stream)), block_size_(block_size) {}
 
-auto DiskReader::block_size() const noexcept -> std::uint64_t { return block_size_; }
+auto DiskReader::get_block_size() const noexcept -> std::uint64_t { return block_size_; }
 
-void DiskReader::set_block_size(BlockSize block_size) noexcept { block_size_ = block_size.value; }
+void DiskReader::set_block_size(std::uint64_t block_size) noexcept { block_size_ = block_size; }
 
 auto DiskReader::read_block() const -> std::vector<std::byte> {
-  is_->seekg(static_cast<std::streamoff>(offset() + handled_size()));
+  is_->seekg(static_cast<std::streamoff>(get_offset() + get_handled_size()));
 
   std::vector<std::byte> block;
-  block.reserve(block_size());
+  block.reserve(get_block_size());
 
   std::istreambuf_iterator<char> iter(*is_);
   std::istreambuf_iterator<char> end;
 
-  for (std::size_t i = 0; i < block_size() && iter != end; ++i, ++iter) {
+  for (std::size_t i = 0; i < get_block_size() && iter != end; ++i, ++iter) {
     block.push_back(static_cast<std::byte>(*iter));
   }
 
@@ -28,5 +28,3 @@ auto DiskReader::read_next_block() -> std::vector<std::byte> {
   increase_handled_size(block.size());
   return block;
 }
-
-DiskReader::BlockSize::BlockSize(std::uint64_t value) : value(value) {}
--- src/FileSystem/DiskHandler/DiskReader/DiskReader.hpp
+++ src/FileSystem/DiskHandler/DiskReader/DiskReader.hpp
@@ -11,20 +11,10 @@ class DiskReader : public DiskHandler {
   std::uint64_t block_size_;
 
 public:
-  struct BlockSize {
-    std::uint64_t value;
+  DiskReader(std::unique_ptr<std::istream> stream, std::uint64_t offset, std::uint64_t block_size);
 
-    explicit BlockSize(std::uint64_t value);
-  };
-
-  static const std::uint64_t DEFAULT_BLOCK_SIZE = 1024;
-
-  DiskReader(std::unique_ptr<std::istream> stream, DiskOffset offset,
-             BlockSize block_size = BlockSize(DEFAULT_BLOCK_SIZE));
-
-  [[nodiscard]] auto block_size() const noexcept -> std::uint64_t;
-
-  void set_block_size(BlockSize block_size) noexcept;
+  [[nodiscard]] auto get_block_size() const noexcept -> std::uint64_t;
+  void set_block_size(std::uint64_t block_size) noexcept;
 
   [[nodiscard]] auto read_block() const -> std::vector<std::byte>;
   auto read_next_block() -> std::vector<std::byte>;
