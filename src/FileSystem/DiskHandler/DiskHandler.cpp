5035d2893415170fbf8fdb8c14c724fbbbb426bf
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-12-09 18:50:43 +0300

    Refactor DiskHandler class

--- src/FileSystem/DiskHandler/DiskHandler.cpp
+++ src/FileSystem/DiskHandler/DiskHandler.cpp
@@ -1,16 +1,14 @@
 #include "DiskHandler.hpp"
 
-DiskHandler::DiskHandler(DiskOffset offset) : offset_(offset.value) {}
+DiskHandler::DiskHandler(std::uint64_t offset) : offset_(offset) {}
 
-auto DiskHandler::offset() const noexcept -> std::uint64_t { return offset_; }
+auto DiskHandler::get_offset() const noexcept -> std::uint64_t { return offset_; }
 
-auto DiskHandler::handled_size() const noexcept -> std::uint64_t { return handled_size_; }
+auto DiskHandler::get_handled_size() const noexcept -> std::uint64_t { return handled_size_; }
 
-void DiskHandler::set_offset(DiskOffset offset) noexcept {
-  offset_ = offset.value;
+void DiskHandler::set_offset(std::uint64_t offset) noexcept {
+  offset_ = offset;
   handled_size_ = 0;
 }
 
 void DiskHandler::increase_handled_size(std::uint64_t size) noexcept { handled_size_ += size; }
-
-DiskHandler::DiskOffset::DiskOffset(std::uint64_t value) : value(value) {}
--- src/FileSystem/DiskHandler/DiskHandler.hpp
+++ src/FileSystem/DiskHandler/DiskHandler.hpp
@@ -8,21 +8,13 @@ class DiskHandler {
   std::uint64_t handled_size_ = 0;
 
 public:
-  struct DiskOffset;
+  explicit DiskHandler(std::uint64_t offset);
 
-  explicit DiskHandler(DiskOffset offset);
+  [[nodiscard]] auto get_offset() const noexcept -> std::uint64_t;
+  [[nodiscard]] auto get_handled_size() const noexcept -> std::uint64_t;
 
-  [[nodiscard]] auto offset() const noexcept -> std::uint64_t;
-  [[nodiscard]] auto handled_size() const noexcept -> std::uint64_t;
-
-  void set_offset(DiskOffset offset) noexcept;
+  void set_offset(std::uint64_t offset) noexcept;
 
 protected:
   void increase_handled_size(std::uint64_t size) noexcept;
-};
-
-struct DiskHandler::DiskOffset {
-  std::uint64_t value;
-
-  explicit DiskOffset(std::uint64_t value);
 };
