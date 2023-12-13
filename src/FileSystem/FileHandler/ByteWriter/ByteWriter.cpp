d8f5a5cb62f74d6db773a5ef8bc5bab35b1de34c
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-12-14 01:57:45 +0300

    Refactor ByteWriter::write_bytes function to fix bugs

--- src/FileSystem/FileHandler/ByteWriter/ByteWriter.cpp
+++ src/FileSystem/FileHandler/ByteWriter/ByteWriter.cpp
@@ -4,22 +4,27 @@ ByteWriter::ByteWriter(ClusterWriter cluster_writer, FAT fat, std::uint64_t clus
     : cluster_writer_(std::move(cluster_writer)), fat_(std::move(fat)), cluster_(cluster) {}
 
 auto ByteWriter::write_bytes(std::uint64_t offset, const std::vector<std::byte> &bytes) -> std::uint64_t {
-  std::uint64_t bytes_written = 0;
-  std::uint64_t file_position = 0;
-  auto cluster_size = cluster_writer_.get_cluster_size();
+  auto cluster_number = offset / cluster_writer_.get_cluster_size();
 
   auto cur_cluster = cluster_;
-  while (bytes_written < bytes.size()) {
-    if (file_position + cluster_size < offset) {
-      file_position += cluster_size;
+  for (std::uint64_t i = 0; i < cluster_number; ++i) {
+    if (fat_.is_last(cur_cluster)) {
+      cur_cluster = fat_.allocate_next(cur_cluster);
     } else {
-      std::uint64_t position = file_position < offset ? offset - file_position : 0;
-      auto bytes_to_write =
-          std::vector<std::byte>(bytes.begin() + static_cast<std::int64_t>(bytes_written), bytes.end());
-      bytes_written += cluster_writer_.write_at_position(cur_cluster, position, bytes_to_write);
-
-      file_position += position + bytes_written;
+      cur_cluster = fat_.get_next(cur_cluster);
     }
+  }
+
+  auto bytes_written = std::uint64_t{0};
+  auto position = offset % cluster_writer_.get_cluster_size();
+  auto bytes_to_write = bytes;
+
+  while (bytes_written < bytes.size()) {
+    auto bytes_written_in_cluster = cluster_writer_.write_at_position(cur_cluster, position, bytes_to_write);
+    bytes_written += bytes_written_in_cluster;
+    position = 0;
+    bytes_to_write = std::vector<std::byte>(
+        bytes_to_write.begin() + static_cast<std::int64_t>(bytes_written_in_cluster), bytes_to_write.end());
 
     if (bytes_written < bytes.size()) {
       if (fat_.is_last(cur_cluster)) {
@@ -30,5 +35,5 @@ auto ByteWriter::write_bytes(std::uint64_t offset, const std::vector<std::byte> 
     }
   }
 
-  return file_position;
+  return bytes_written + offset;
 }
